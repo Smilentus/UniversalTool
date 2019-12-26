27ade46df834cb04e498062697cd6b725458b357
Smilentus <gilev.dimon@mail.ru>
2019-12-26 21:17:44 +0500

    v0.1 Универсальная штука для преобразования текста в различные форматы

---
+++ start.py
@@ -0,0 +1,25 @@
+origin = input('Введите текст: ')
+
+engAl = 'abcdefghijklmnopqrstuvwxyz'
+rusAl = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+# TODO:
+# Транслитерация
+# Численное представление
+# Двоичный код
+# Ещё что-то придумаю
+
+# Место для функций
+transText = ''
+
+digitText = ''
+
+binaryText = ''
+
+# Итоговый вывод
+print(f'Оригинальная строка: {origin}')
+print(f'')
+print(f'')
+print(f'')
+print(f'')
+input('Нажмите Enter для завершения ...')
